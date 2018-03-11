f65aa0f0fda30d77001a0b7a30710c21b5811d36
VasyaBilak <vasyabilak.vb@gmail.com>
2018-03-11 15:30:38 +0200

    14

--- app/car/car.component.ts
+++ app/car/car.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-car',
@@ -7,6 +7,8 @@ import { Component } from '@angular/core';
 })
 export class CarComponent  {
 
-  
+ @Input('carItem') car: {name: string, year: number};
+
+ private carItem = false;
 
 }
--- app/cars/cars.component.ts
+++ app/cars/cars.component.ts
@@ -7,21 +7,35 @@ import { Component } from '@angular/core';
 })
 export class CarsComponent {
 
-addCarStatus = false;
 carName = '';
-cars = ['Ford', 'Audi', 'BMW'];
-items = [{id: 3, name: 'item 1' },{id: 6, name: 'item 2'}, {id: 9, name: 'item 3'} ];
+carYear = 2017;
+
+cars: [{name: string, year: number}] = [{
+  name: 'Ford',
+  year: 2015
+}, {
+  name: 'Mazda',
+  year: 2010
+}, {
+  name: 'Audi',
+  year: 2017
+}
+
+];
 
 constructor() {
 
 }
 
-addCar() {
-  this.addCarStatus = true;
-  this.cars.push(this.carName);
-  this.carName = '';
-}
 
+addCar(){
+  this.cars.push({
+    name: this.carName,
+    year: this.carYear
+  });
+  this.carName = '';
+  this.carYear = 2017;
+}
 
 
 }
